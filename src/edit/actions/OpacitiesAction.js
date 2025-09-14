ba8131d10dbd0ffbfd599efde7ac90c9c890091a
Léni Vienne <leni.vienne@developpement-durable.gouv.fr>
2025-09-14 14:13:52 +0200

    more tests

--- src/edit/actions/OpacitiesAction.js
+++ src/edit/actions/OpacitiesAction.js
@@ -104,7 +104,7 @@ L.OpacitiesAction = L.EditAction.extend({
     options.toolbarIcon = {
       svg: true,
       html: 'opacities',
-      tooltip: 'Set custom opacity',
+      tooltip: 'Set custom opacity23322332',
       className: mode === 'lock' ? 'disabled' : '',
     };
 
